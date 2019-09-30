d0a02bfc573ff11e04dcf157ed554340472d84a7
Maysam Rabipour <maysam.rabipour@gmail.com>
2019-09-30 22:44:40 +0330

    Initial Commit

---
+++ routes/api/users.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+
+// @route   GET api/users/test
+// @desc    Tests users router
+// @access  Public
+router.get('/test', (req, res) => res.json({msg: "Users Module Works"}))
+
+
+module.exports = router
